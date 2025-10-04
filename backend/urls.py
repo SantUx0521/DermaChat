ee6ceabeb9eb96e34070937f05a5cd1b9747c3d1
SantUx0521 <santiago.useche@correounivalle.edu.co>
2025-10-04 11:20:53 -0500

    cambios importantes

--- backend/urls.py
+++ backend/urls.py
@@ -1,3 +1,5 @@
+from django.contrib import admin
+from django.urls import path, include
 """
 URL configuration for DermaChat project.
 
@@ -19,4 +21,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
